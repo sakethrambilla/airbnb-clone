700c767782bc4296e57e176b8b6b37bcad272604
Indrarjun <billasaketh@gmail.com>
2023-09-04 16:51:51 +0530

    Error was corrected

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,20 @@
 import "./App.css";
-import {Route,Router,Routes} from "react-router-dom";
+import { Route, Router, Routes } from "react-router-dom";
 import IndexPage from "./pages/IndexPage";
 import Loginpage from "./pages/LoginPage";
+import Registerpage from "./pages/RegisterPage";
 import Layout from "./Layout";
-
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Layout/>}>
-      <Route index element={<IndexPage/>}/>
-      <Route path="/login" element={<Loginpage/>}/>
-      <Route path="/register" element={<Registerpage/>}/>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<IndexPage />} />
+        <Route path="/login" element={<Loginpage />} />
+        <Route path="/register" element={<Registerpage />} />
       </Route>
     </Routes>
-    
-  )
+  );
 }
 
 export default App;
